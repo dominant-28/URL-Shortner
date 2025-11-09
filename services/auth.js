ae4b31426e50044a93596b70b592b499f3ac5c65
SOHAM KALE <157865836+dominant-28@users.noreply.github.com>
2025-11-09 09:47:03 +0530

    Add files via upload

---
+++ services/auth.js
@@ -0,0 +1,17 @@
+const jwt=require("jsonwebtoken")
+const secret ="soham@#$"
+function setuser( user) {
+
+  return jwt.sign({
+    _id:user._id,
+    email:user.email,
+  },secret)
+}
+function getuser(token) {
+  if(!token) return null;
+  return jwt.verify(token,secret);
+}
+module.exports = {
+  setuser,
+  getuser,
+};
